ae65567533472553699d2a9c5167a4ba47208ebc
Lucas Kenji <lmurakami98@gmail.com>
2023-11-26 17:52:27 -0300

    change product instantiation method

--- src/app/models/Product.model.ts
+++ src/app/models/Product.model.ts
@@ -1,8 +1,12 @@
 export class Product {
-  constructor(
-    public id: number,
+  private constructor(
+    public id: string,
     public name: string,
     public description: string,
     public price: number
   ) {}
+
+  static create(name: string, description: string, price: number): Product {
+    return new this(window.crypto.randomUUID(), name, description, price);
+  }
 }
